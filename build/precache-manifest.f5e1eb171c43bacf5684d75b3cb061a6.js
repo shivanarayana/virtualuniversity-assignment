98c28a7dc6791f29ae4e9bfd40f78116d04588f2
venkata shiva narayana modini <shivanarayana@users.noreply.github.com>
2020-05-06 11:08:04 +0530

    Add files via upload

---
+++ build/precache-manifest.f5e1eb171c43bacf5684d75b3cb061a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d00e30817c21fca7f67e9bb776412e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca7cc24ef0de8aa44e1f",
+    "url": "/static/css/2.ce9f3f1e.chunk.css"
+  },
+  {
+    "revision": "ed013c2bdecfada44dd1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ca7cc24ef0de8aa44e1f",
+    "url": "/static/js/2.0b8481c3.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.0b8481c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed013c2bdecfada44dd1",
+    "url": "/static/js/main.d9c3ffe9.chunk.js"
+  },
+  {
+    "revision": "8e16c111d6ad10cb4b1c",
+    "url": "/static/js/runtime-main.f31b6860.js"
+  }
+]);
